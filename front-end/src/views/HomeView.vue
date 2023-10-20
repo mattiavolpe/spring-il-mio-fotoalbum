abc16ee93df632667627e8b0f82c1cd8932b4992
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-10-20 11:50:59 +0200

    ADD loader and last research indication

--- front-end/src/views/HomeView.vue
+++ front-end/src/views/HomeView.vue
@@ -9,40 +9,51 @@ const photos = ref([]);
 
 const filter = ref("");
 
+const lastResearch = ref("");
+
+const loading = ref(true);
+
 function searchPhotos() {
-  if (filter.value != "")
-    fetchPhotos(filter.value);
-  else
-    fetchAll();
+  if (filter.value === "")
+    return;
+  
+  fetchPhotos(filter.value);
 }
 
 function fetchPhotos(keyword) {
+  loading.value = true;
+  lastResearch.value = keyword;
   axios
   .get(`${API_URL}?filter=${keyword}`)
   .then(res => {
-    if (res.data.length > 0) {
+    if (res.status == 200)
       photos.value = res.data;
-      filter.value = "";
-    }
-    else {
+    else
       photos.value = [];
-      filter.value = "";
-    }
   })
   .catch(error => {
     console.error(error);
+  })
+  .finally(() => {
+    loading.value = false;
   })
 }
 
 function fetchAll() {
+  loading.value = true;
   axios
   .get(API_URL)
   .then(res => {
-    if (res.data.length > 0)
+    if (res.status == 200)
       photos.value = res.data;
+    else
+      photos.value = [];
   })
   .catch(error => {
     console.error(error);
+  })
+  .finally(() => {
+    loading.value = false;
   })
 }
 
@@ -54,16 +65,25 @@ onMounted(() => {
 
 <template>
   <main class="py-5">
-    <div class="container">
+    <div v-if="loading" class="container text-secondary text-center">
+      <box-icon name='hourglass' animation="spin" size="lg" id="loading_photos_icon"></box-icon>
+      <h2>LOADING PHOTOS</h2>
+    </div>
+
+    <div v-if="photos.length > 0" class="container">
       
       <form v:if="photos.length > 0" class="mb-4" @submit.prevent="searchPhotos()">
-        <label for="filter" class="text-light">Search</label>
-        <br />
-        <div id="search" class="d-flex flex-column align-items-start">
-          <input class="my-3 px-1 py-2 rounded border border-light text-light" type="text" name="filter" id="filter" placeholder="Type a filter..." v-model.trim="filter" />
+        <label for="filter" class="text-light mb-2">Search</label>
+        <div id="search" class="d-flex flex-column align-items-start gap-3">
+          <input class="px-1 py-2 rounded border border-light text-light" type="text" name="filter" id="filter" placeholder="Type a filter..." v-model.trim="filter" />
           <button class="btn btn-outline-light px-3 py-2 rounded" type="submit">Search</button>
+          <button v-if="lastResearch != ''" class="btn btn-outline-danger px-3 py-2 rounded" @click="fetchAll(); filter = ''; lastResearch = ''">All photos</button>
         </div>
       </form>
+
+      <h3 v-if="lastResearch != ''" class="text-center text-light">
+        Search results for "{{ lastResearch }}"
+      </h3>
       
       <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
         <div v-for="photo in photos" class="col">
@@ -86,6 +106,12 @@ onMounted(() => {
         </div>
       </div>
       
+    </div>
+
+    <div v-if="photos.length < 1 && !loading" class="container text-secondary text-center">
+      <box-icon name='camera-off' size="lg" id="no_photos_icon"></box-icon>
+      <h1 color="black">NO PHOTOS AT THE MOMENT</h1>
+      <h3>COME BACK TO CHECK LATER</h3>
     </div>
   </main>
 </template>
