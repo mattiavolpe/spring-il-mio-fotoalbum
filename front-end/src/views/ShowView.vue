3b7157482eeb8c16c731ee4ae23100f2233cb69f
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-10-20 15:25:10 +0200

    CHANGE category link underline style

--- front-end/src/views/ShowView.vue
+++ front-end/src/views/ShowView.vue
@@ -74,8 +74,8 @@ onMounted(() => {
       <div v:if="photo.categories.length > 0" class="bg-dark mt-4 p-3 rounded">
         <h4 class="m-0 text-light">Categories:</h4>
         <span v-for="(category, index) in photo.categories">
-          <RouterLink :to="{ name: 'categoryShow', params: { id: category.id}}" v-if="index < photo.categories.length - 1 && photo.categories.length > 1" class="fst-italic text-secondary">
-            {{category.name}} - 
+          <RouterLink :to="{ name: 'categoryShow', params: { id: category.id}}" v-if="index < photo.categories.length - 1 && photo.categories.length > 1" class="fst-italic text-secondary text-decoration-none">
+            <span class="text-decoration-underline">{{category.name}}</span> - 
           </RouterLink>
         </span>
         <span v-for="(category, index) in photo.categories">
